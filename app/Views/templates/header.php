7d9b96b5e603aa7184fb3db109225d2a016f010d
CHELSEA\qa860 <qa860605@gmail.com>
2026-06-01 11:56:58 +0800

    feat: 新增 Bootstrap Icons 樣式表以增強頁面圖示顯示

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -3,6 +3,7 @@
 <head>
     <title>That BookStore.</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <?php if (isset($page_css) && is_array($page_css)): ?>
         <?php foreach ($page_css as $css): ?>
             <link rel="stylesheet" href="<?= (strpos($css, 'http') === 0) ? $css : base_url($css) ?>">
