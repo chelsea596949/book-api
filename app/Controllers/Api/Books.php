8d56bae8ca406f7f01978f01525014ec2c3984a1
CHELSEA\qa860 <qa860605@gmail.com>
2026-06-18 12:42:41 +0800

    fix_bug(編輯書籍)

--- app/Controllers/Api/Books.php
+++ app/Controllers/Api/Books.php
@@ -1,9 +1,7 @@
 <?php
-
 namespace App\Controllers\Api;
 
 use App\Controllers\BaseController;
-// use App\Transformers\BookTransformer;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Api\ResponseTrait;
 use App\DTO\Book\BookQueryDTO;
@@ -38,7 +36,7 @@ class Books extends BaseController
      */
     public function putIndex(int $id): ResponseInterface
     {
-        $data = $this->request->getPost();
+        $data = $this->request->getRawInput();
 
         // 改用validateData()來驗證合併後的陣列
         if(!$this->validateData($data, BookEditDTO::rules())) {
@@ -46,16 +44,16 @@ class Books extends BaseController
                 $this->response,
                 api_error(
                     'Validation failed',
-                    $this->validator->getErrors(), // 這裡現在絕對能噴出 max_size 的錯誤訊息了！
+                    $this->validator->getErrors(),
                     400
                 )
             );
         }
 
-        // 取得圖片檔案（傳入在HTML input的name，例如book_image）
+        // 取得圖片檔案（傳入在HTML input的name, 例如book_image）
         $file = $this->request->getFile('book_image');
         
-        // 檢查檔案是否「真的有上傳成功」且「沒有損壞或被移動」
+        // 檢查檔案是否真的有上傳成功且沒有損壞或被移動
         if($file && $file->isValid() && !$file->hasMoved()) {
             // 呼叫FileService上傳
             $imageName = service('fileService')->uploadImage($file);
@@ -65,7 +63,7 @@ class Books extends BaseController
             }
         }
         
-        // 將合併了圖片名稱的$data封裝進DTO並執行 Service
+        // 將合併了圖片名稱的$data封裝進DTO並執行Service
         try {
             $dto = BookEditDTO::fromArray($data);
             $responseDTO = service('bookService')->editBook($dto, $id);
@@ -103,7 +101,7 @@ class Books extends BaseController
             );
         }
 
-        // 使用 FileService 處理圖片
+        // 使用FileService處理圖片
         $imageName = service('fileService')->uploadImage($this->request->getFile('book_image'));
         if($imageName) {
             $data['image_url'] = $imageName;
